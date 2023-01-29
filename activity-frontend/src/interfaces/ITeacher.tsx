de8e8e2fba16406ab61fb74b6ddd6f1169b4d592
Soporte1 <soporte1@iconta.ec>
2023-01-28 23:12:21 -0500

    model Teacher

---
+++ activity-frontend/src/interfaces/ITeacher.tsx
@@ -0,0 +1,9 @@
+export interface Teacher{
+    idTeacher:number;
+    emailTeacher:string;
+    passwordTeacher:string;
+    identityNumber:string;
+    names:string;
+    lastNames:string;
+    faceid:string;
+}
