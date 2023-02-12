c0f82030de8a13785c757c2304facab3b1fcc671
Soporte1 <soporte1@iconta.ec>
2023-02-12 09:31:11 -0500

    Ruta para la pantalla

--- activity-frontend/src/routes/AppRoutes.tsx
+++ activity-frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { ActivitiesScreen } from '../screens/activities/ActivitiesScreen';
 import { LoginScreen } from '../screens/login/LoginScreen';
 import { RegisterScreen } from '../screens/register/RegisterScreen';
 export const AppRoutes = ()=>{
@@ -6,6 +7,7 @@ export const AppRoutes = ()=>{
         <Routes>
             <Route path='/' element={<RegisterScreen/>}/>
             <Route path='/login' element={<LoginScreen/>}/>
+            <Route path='/activities' element={<ActivitiesScreen/>}/>
         </Routes>
     );
 }
---
+++ activity-frontend/src/screens/activities/ActivitiesScreen.tsx
@@ -0,0 +1,3 @@
+export const ActivitiesScreen = () => {
+    return (<div></div>);
+};
