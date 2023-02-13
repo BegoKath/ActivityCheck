2f461c907ad7ea3e0c468e7b36a915ade57444e9
Soporte1 <soporte1@iconta.ec>
2023-02-13 10:50:44 -0500

    Creación del archivo para requerimientos

---
+++ activity-backend/CustomerWebApi/Models/Requeriments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerWebApi.Models
+{
+    public class RequerimentsLogin
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
