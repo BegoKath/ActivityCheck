d00184f69e25e26ce61f2a7ec7983302fee3a0bd
Soporte1 <soporte1@iconta.ec>
2023-02-21 21:31:42 -0500

    header

--- activity-frontend/src/screens/admin/AdminScreen.tsx
+++ activity-frontend/src/screens/admin/AdminScreen.tsx
@@ -1,10 +1,17 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { NavBar } from "./components/NavBar";
 
 export const AdminScreen = () => {
   return (
     <Container style={page}>
       <NavBar/>
+      <div style={head}>
+        <Row>Control de actividades</Row>
+        <Row style={{ fontSize: "15px" }}>
+          Carrera de Ingeniería de Software
+        </Row>
+      </div>
+      
     </Container>
   );
 };
@@ -17,3 +24,12 @@ const page: React.CSSProperties = {
   background: "#F9F8F0",
   padding:0,
 };
+const head: React.CSSProperties = {
+    minHeight: "150px",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "30px",
+  };
